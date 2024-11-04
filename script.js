de496d6010e40f29b1155d9f9c93460edd4e3e1e
simhq <hanqiangsim@gmail.com>
2024-11-04 07:29:03 +0000

    bug fix

---
+++ script.js
@@ -0,0 +1,24 @@
+var age = 20;
+age = 65;
+console.log(age);
+
+var username = "simhq";
+var password = "password";
+var isMale = true;
+var isLoggedIn = false;
+
+var blog_posts = ["hello, how are you", "i am well", "thank you"];
+console.log(blog_posts);
+console.log(isMale);
+
+if (isMale == false) {
+    console.log("You are male");
+}
+else {
+    console.log("You are a girl");
+}
+
+while (age < 70) {
+    console.log("My age is " + age)
+    age++;
+}
